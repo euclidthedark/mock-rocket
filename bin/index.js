a7d319764b34387e4bd3787b07c4473b53acf14e
euclidthedark <euclidthedark@gmail.com>
2022-07-16 19:20:02 -0400

    create hello world file

---
+++ bin/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const helloWorld = 'Hello World!';
+function printF(s) {
+    return console.log(s);
+}
+printF(helloWorld);
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const helloWorld: string = 'Hello World!';
+
+
+function printF(s: string) {
+  return console.log(s);
+}
+
+printF(helloWorld);
+
